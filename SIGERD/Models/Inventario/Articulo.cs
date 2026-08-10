c5d77517a1c295050826751585dc9e2630147637
JordysDev <jordys@correo.com>
2026-08-10 13:39:33 -0600

    ajuste de los modelos de categoria y articulos agregando el campo estado para activar e inactivar

--- SIGERD/Models/Inventario/Articulo.cs
+++ SIGERD/Models/Inventario/Articulo.cs
@@ -7,6 +7,7 @@ namespace SIGERD.Models.Inventario
         public int idArticulo { get; set; }
         public string? nombreArticulo { get; set; }
         public string? descripcion { get; set; }
+        public bool estado { get; set; }
         public int idCategoriaArticulo { get; set; }
         public Categoria? Categoria { get; set; }
         public ICollection<DetalleEnvio> DetallesEnvio { get; set; } = new List<DetalleEnvio>();
--- SIGERD/Models/Inventario/Categoria.cs
+++ SIGERD/Models/Inventario/Categoria.cs
@@ -3,7 +3,11 @@
     public class Categoria
     {
         public int idCategoria { get; set; }
-        public string nombreCategoria { get; set; }
+
+        public string nombreCategoria { get; set; } = string.Empty;
+
+        public bool estado { get; set; }
+
         public ICollection<Articulo> Articulos { get; set; } = new List<Articulo>();
     }
 }
